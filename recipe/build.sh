cc0fc771e8aa4d0d205fb097ed8c7a59cea703b8
Mark Harfouche <mark.harfouche@gmail.com>
2020-05-14 19:20:03 -0400

    Try vanialla settings for wxWidgets

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,5 @@
 set -ex
 
-# For some reason, we cannot find WX_libraries easily
-WX_LIBS=`wx-config --libs`
-export LIBS="${LIBS} ${WX_LIBS}"
-export LDFLAGS="${LDFLAGS} ${WX_LIBS}"
-
 mkdir build
 cd build
 
@@ -12,7 +7,6 @@ cmake -LAH                              \
     -DCMAKE_BUILD_TYPE="Release"        \
     -DCMAKE_PREFIX_PATH=${PREFIX}       \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}    \
-    -DwxWidgets_LIBRARIES=${PREFIX}/include   \
     ..
 
 make VERBOSE=1 V=1
