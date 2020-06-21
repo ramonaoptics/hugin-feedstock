dbd2e340ccf470ecdc27d1d623a59e580aa2b223
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-20 20:43:19 -0400

    Try building for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,11 @@
-REM Nothing for now
+mkdir build
+cd build
+
+cmake -LAH -G Ninja                     ^
+    -DCMAKE_BUILD_TYPE="Release"        ^
+    -DCMAKE_PREFIX_PATH=%PREFIX%        ^
+    -DCMAKE_INSTALL_PREFIX=%PREFIX%     ^
+    ..
+
+ninja install
+
