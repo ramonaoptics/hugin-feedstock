733b40e5139a5a9e5ac03215c5872c059043e8e8
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-21 08:05:19 -0400

    Add CMakeLists.txt with more debugging info

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,18 @@
+copy %RECIPE_DIR%\CMakeLists.txt .
+if errorlevel 1 exit 1
+
 mkdir build
+if errorlevel 1 exit 1
 cd build
+if errorlevel 1 exit 1
 
 cmake -LAH -G Ninja                     ^
     -DCMAKE_BUILD_TYPE="Release"        ^
     -DCMAKE_PREFIX_PATH=%PREFIX%        ^
     -DCMAKE_INSTALL_PREFIX=%PREFIX%     ^
     ..
+if errorlevel 1 exit 1
 
 ninja install
+if errorlevel 1 exit 1
 
