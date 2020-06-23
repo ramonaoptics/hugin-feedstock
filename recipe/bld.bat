26e644bbfa83f7665e15888a7620d81fb379669f
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-23 15:59:04 -0400

    Try to specifcy the library_prefix instead

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,23 +11,25 @@ if errorlevel 1 exit 1
 
 cmake -LAH -G Ninja                           ^
     -DCMAKE_BUILD_TYPE="Release"              ^
-    -DCMAKE_PREFIX_PATH=%PREFIX%              ^
-    -DCMAKE_INSTALL_PREFIX=%PREFIX%           ^
+    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%      ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%   ^
     -DwxWidgets_ROOT_DIR=%LIBRARY_PREFIX%     ^
     -DwxWidgets_INCLUDE_DIRS=%LIBRARY_INC%\wx ^
     -wxWidgets_LIBRARIES=%LIBRARY_INC%\lib    ^
     -DENBLEND_DIR=%LIBRARY_PREFIX%            ^
     -DOPENEXR_BIN_DIR=%LIBRARY_BIN%           ^
-    -DPANO13_DLL=%LIBRARY_BIN%\pano13.dll     ^
-    -DPANO13_EXE_DIR=%LIBRARY_BIN%            ^
-    -DPNG_DLL=%LIBRARY_BIN%\libpng16.dll      ^
-    -DTIFF_DLL=%LIBRARY_BIN%\libtiff.dll      ^
-    -DEXIFVTOOL_EXE=%LIBRARY_BIN%\exiftool    ^
-    -DEXIV2_DLL=%LIBRARY_BIN%\exiv2.dll       ^
     -DHUGIN_BUILDER="RamonaOptics"            ^
     ..
+
 if errorlevel 1 exit 1
 
 ninja install
 if errorlevel 1 exit 1
 
+
+REM    -DPANO13_DLL=%LIBRARY_BIN%\pano13.dll
+REM    -DPANO13_EXE_DIR=%LIBRARY_BIN%
+REM    -DPNG_DLL=%LIBRARY_BIN%\libpng16.dll
+REM    -DTIFF_DLL=%LIBRARY_BIN%\libtiff.dll
+REM    -DEXIFVTOOL_EXE=%LIBRARY_BIN%\exiftool
+REM    -DEXIV2_DLL=%LIBRARY_BIN%\exiv2.dll
